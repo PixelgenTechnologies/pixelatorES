189792c47a37f11715cf111a71e2c3b1bfaad8ad
maxkarlsson <max.karlsson@pixelgen.com>
2026-04-30 19:25:46 +0200

    feat: load degree distr

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,16 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+### Added 
+
+- Added `get_degree_distribution` to load degree distribution from QC files.
+
+### Changed
+
+- Added new slots to example data jsons with degree distribution. 
+
 ## [0.9.1] 2026-04-30
 
 ### Added
--- tests/testthat/test_key_table.R
+++ tests/testthat/test_key_table.R
@@ -39,29 +39,29 @@ for (data_type in data_types) {
     expect_equal(
       res,
       switch(data_type,
-        default =
-          structure(list(
-            sample_alias = "S1", Q30 = 0.957683900982373,
-            total_reads = 400745924L, deduped_valid_reads = 232530522L,
-            valid_reads = 359952911L, graph_edges = 3171564L, graph_proteins = 1469442L,
-            graph_reads = 8746572L, valid_reads_saturation = 35.3997384396761,
-            graph_edge_saturation = 63.7393483984354, graph_node_saturation = 83.1997953026626,
-            fraction_valid_reads = 89.8207291560625, fraction_graph_reads = 2.18257291620014
-          ), row.names = c(
-            NA,
-            -1L
-          ), class = c("tbl_df", "tbl", "data.frame")),
-        hashing =
-          structure(list(
-            pool = "pool1", total_reads = 2240712L, deduped_valid_reads = 1159113L,
-            valid_reads = 1953716L, graph_edges = 3171564L, graph_proteins = 1469442L,
-            graph_reads = 8746572L, valid_reads_saturation = 40.6713667697864,
-            graph_edge_saturation = 63.7393483984354, graph_node_saturation = 83.1997953026626,
-            fraction_valid_reads = 87.1917497652532, fraction_graph_reads = 390.347889420863
-          ), row.names = c(
-            NA,
-            -1L
-          ), class = c("tbl_df", "tbl", "data.frame"))
+             default =
+               structure(list(
+                 sample_alias = "S1", Q30 = 0.957683900982373,
+                 total_reads = 400745924L, deduped_valid_reads = 232530522L,
+                 valid_reads = 359952911L, graph_edges = 3171564L, graph_proteins = 1469442L,
+                 graph_reads = 8746572L, valid_reads_saturation = 35.3997384396761,
+                 graph_edge_saturation = 63.7393483984354, graph_node_saturation = 83.1997953026626,
+                 fraction_valid_reads = 89.8207291560625, fraction_graph_reads = 2.18257291620014
+               ), row.names = c(
+                 NA,
+                 -1L
+               ), class = c("tbl_df", "tbl", "data.frame")),
+             hashing =
+               structure(list(
+                 pool = "pool1", total_reads = 2240712L, deduped_valid_reads = 1159113L,
+                 valid_reads = 1953716L, graph_edges = 3171564L, graph_proteins = 1469442L,
+                 graph_reads = 8746572L, valid_reads_saturation = 40.6713667697864,
+                 graph_edge_saturation = 63.7393483984354, graph_node_saturation = 83.1997953026626,
+                 fraction_valid_reads = 87.1917497652532, fraction_graph_reads = 390.347889420863
+               ), row.names = c(
+                 NA,
+                 -1L
+               ), class = c("tbl_df", "tbl", "data.frame"))
       )
     )
 
@@ -89,42 +89,63 @@ for (data_type in data_types) {
     expect_equal(
       res,
       switch(data_type,
-        default =
-          structure(
-            list(
-              sample_alias = c("S1", "S1", "S2", "S2"),
-              removed_total = c(7311081L, 7311081L, 3866617L, 3866617L),
-              total_edges_in = c(144111916L, 144111916L, 90118672L, 90118672L),
-              type = c("Initial stage", "Refinement stage", "Initial stage", "Refinement stage"),
-              edges = c(7292268L, 18813L, 3849577L, 17040L),
-              percent = c(
-                5.06014228552759, 0.0130544374970353,
-                4.27167524173015, 0.0189084011357824
-              )
-            ),
-            row.names = c(NA, -4L),
-            class = c("tbl_df", "tbl", "data.frame")
-          ),
-        hashing =
-          structure(list(
-            pool = c("pool1", "pool1", "pool2", "pool2"),
-            removed_total = c(13678L, 13678L, 13564L, 13564L), total_edges_in = c(
-              813120L,
-              813120L, 813117L, 813117L
-            ), type = c(
-              "Initial stage", "Refinement stage",
-              "Initial stage", "Refinement stage"
-            ), edges = c(
-              9910L, 3768L,
-              10008L, 3556L
-            ), percent = c(
-              1.21876229830775, 0.463400236127509,
-              1.23081918100347, 0.437329437215063
-            )
-          ), row.names = c(
-            NA,
-            -4L
-          ), class = c("tbl_df", "tbl", "data.frame"))
+             default =
+               structure(
+                 list(
+                   sample_alias = c("S1", "S1", "S2", "S2"),
+                   removed_total = c(7311081L, 7311081L, 3866617L, 3866617L),
+                   total_edges_in = c(144111916L, 144111916L, 90118672L, 90118672L),
+                   type = c("Initial stage", "Refinement stage", "Initial stage", "Refinement stage"),
+                   edges = c(7292268L, 18813L, 3849577L, 17040L),
+                   percent = c(
+                     5.06014228552759, 0.0130544374970353,
+                     4.27167524173015, 0.0189084011357824
+                   )
+                 ),
+                 row.names = c(NA, -4L),
+                 class = c("tbl_df", "tbl", "data.frame")
+               ),
+             hashing =
+               structure(list(
+                 pool = c("pool1", "pool1", "pool2", "pool2"),
+                 removed_total = c(13678L, 13678L, 13564L, 13564L), total_edges_in = c(
+                   813120L,
+                   813120L, 813117L, 813117L
+                 ), type = c(
+                   "Initial stage", "Refinement stage",
+                   "Initial stage", "Refinement stage"
+                 ), edges = c(
+                   9910L, 3768L,
+                   10008L, 3556L
+                 ), percent = c(
+                   1.21876229830775, 0.463400236127509,
+                   1.23081918100347, 0.437329437215063
+                 )
+               ), row.names = c(
+                 NA,
+                 -4L
+               ), class = c("tbl_df", "tbl", "data.frame"))
+      )
+    )
+
+    # Degree distribution
+
+    expect_no_error(res <- get_degree_distribution(sample_qc_metrics))
+    expect_equal(
+      head(res),
+      switch (data_type,
+              default =
+                structure(list(sample_alias = c("S1", "S1", "S1", "S1", "S1",
+                                                "S1"), umi_type = c("umi1", "umi1", "umi1", "umi1", "umi1", "umi1"
+                                                ), degree = c("1", "2", "3", "4", "5", "6"), n = c(27439568L,
+                                                                                                   7545696L, 5571202L, 4951939L, 4609669L, 4290856L)), row.names = c(NA,
+                                                                                                                                                                     -6L), class = c("tbl_df", "tbl", "data.frame")),
+              hashing =
+                structure(list(pool = c("pool1", "pool1", "pool1", "pool1", "pool1",
+                                        "pool1"), umi_type = c("umi1", "umi1", "umi1", "umi1", "umi1",
+                                                               "umi1"), degree = c("1", "2", "3", "4", "5", "6"), n = c(75201L,
+                                                                                                                        55220L, 45622L, 33576L, 22807L, 14510L)), row.names = c(NA, -6L
+                                                                                                                        ), class = c("tbl_df", "tbl", "data.frame"))
       )
     )
 
@@ -133,27 +154,27 @@ for (data_type in data_types) {
     expect_equal(
       res,
       switch(data_type,
-        default =
-          structure(
-            list(
-              sample_alias = c("S1", "S2"),
-              ratio = c(2.67032153250738, 4.85786823694461),
-              number_of_umis_removed = c(1621394L, 2724990L)
-            ),
-            class = c("tbl_df", "tbl", "data.frame"),
-            row.names = c(NA, -2L)
-          ),
-        hashing =
-          structure(list(sample_alias = c("S1", "S11", "S12", "S2"), ratio = c(
-            4.78266613819527,
-            4.78980422103143, 4.73014521545811, 4.6124845823863
-          ), number_of_umis_removed = c(
-            3620L,
-            3638L, 6000L, 5946L
-          )), row.names = c(NA, -4L), class = c(
-            "tbl_df",
-            "tbl", "data.frame"
-          ))
+             default =
+               structure(
+                 list(
+                   sample_alias = c("S1", "S2"),
+                   ratio = c(2.67032153250738, 4.85786823694461),
+                   number_of_umis_removed = c(1621394L, 2724990L)
+                 ),
+                 class = c("tbl_df", "tbl", "data.frame"),
+                 row.names = c(NA, -2L)
+               ),
+             hashing =
+               structure(list(sample_alias = c("S1", "S11", "S12", "S2"), ratio = c(
+                 4.78266613819527,
+                 4.78980422103143, 4.73014521545811, 4.6124845823863
+               ), number_of_umis_removed = c(
+                 3620L,
+                 3638L, 6000L, 5946L
+               )), row.names = c(NA, -4L), class = c(
+                 "tbl_df",
+                 "tbl", "data.frame"
+               ))
       )
     )
 
@@ -238,8 +259,8 @@ for (data_type in data_types) {
               "0a45497c6bfbfb22_5", "0a45497c6bfbfb22_6"
             ),
             .rows = structure(list(1L, 2L, 3L, 4L, 5L, 6L),
-              ptype = integer(0),
-              class = c("vctrs_list_of", "vctrs_vctr", "list")
+                              ptype = integer(0),
+                              class = c("vctrs_list_of", "vctrs_vctr", "list")
             )
           ),
           class = c("tbl_df", "tbl", "data.frame"),
@@ -281,53 +302,53 @@ for (data_type in data_types) {
     expect_equal(
       tabl$sample,
       switch(data_type,
-        default =
-          structure(
-            list(
-              `Sample ID` = "S1",
-              `Number of cells` = "30",
-              `Number of cells >10k nodes` = "30",
-              `Median isotype % counts` = "0.04",
-              `Median intracellular % counts` = "0",
-              `Median proteins per cell [k]` = "48.64",
-              `Median reads per cell [k]` = "289.86",
-              `Q30 [%]` = "95.77",
-              `Total reads [M]` = "400.75",
-              `Valid reads [M]` = "359.95",
-              `Graph Nodes [M]` = "1.47",
-              `Graph Edges [M]` = "3.17",
-              `Graph node saturation [%]` = "83.2",
-              `Graph edge saturation [%]` = "63.74",
-              `Valid reads saturation [%]` = "35.4",
-              `Valid reads fraction [%]` = "89.82",
-              `Graph reads fraction [%]` = "2.18",
-              `% Denoised UMIs` = "2.67",
-              `Total denoised UMIs [M]` = "1.62",
-              `Median mean coreness` = "2.47",
-              `Median % dangling nodes` = "24.99",
-              `Median % well connected nodes` = "57.11",
-              `Top 3 % counts` = "35.2",
-              `Top 5 % counts` = "45.23",
-              `Top 5 markers` = "CD44, B2M, CD59, CD45, CD43",
-              `% Crossing edges (Initial)` = "5.06",
-              `% Crossing edges (Refinement)` = "0.01"
-            ),
-            row.names = c(NA, -1L),
-            class = c("tbl_df", "tbl", "data.frame")
-          ),
-        hashing =
-          structure(list(
-            `Sample ID` = "S1", `Number of cells` = "30",
-            `Number of cells >10k nodes` = "30", `Median isotype % counts` = "0.04",
-            `Median intracellular % counts` = "0", `Median proteins per cell [k]` = "48.64",
-            `Median reads per cell [k]` = "289.86", `% Denoised UMIs` = "4.78",
-            `Total denoised UMIs [M]` = "0", `Median mean coreness` = "2.47",
-            `Median % dangling nodes` = "24.99", `Median % well connected nodes` = "57.11",
-            `Top 3 % counts` = "35.2", `Top 5 % counts` = "45.23", `Top 5 markers` = "CD44, B2M, CD59, CD45, CD43"
-          ), row.names = c(
-            NA,
-            -1L
-          ), class = c("tbl_df", "tbl", "data.frame"))
+             default =
+               structure(
+                 list(
+                   `Sample ID` = "S1",
+                   `Number of cells` = "30",
+                   `Number of cells >10k nodes` = "30",
+                   `Median isotype % counts` = "0.04",
+                   `Median intracellular % counts` = "0",
+                   `Median proteins per cell [k]` = "48.64",
+                   `Median reads per cell [k]` = "289.86",
+                   `Q30 [%]` = "95.77",
+                   `Total reads [M]` = "400.75",
+                   `Valid reads [M]` = "359.95",
+                   `Graph Nodes [M]` = "1.47",
+                   `Graph Edges [M]` = "3.17",
+                   `Graph node saturation [%]` = "83.2",
+                   `Graph edge saturation [%]` = "63.74",
+                   `Valid reads saturation [%]` = "35.4",
+                   `Valid reads fraction [%]` = "89.82",
+                   `Graph reads fraction [%]` = "2.18",
+                   `% Denoised UMIs` = "2.67",
+                   `Total denoised UMIs [M]` = "1.62",
+                   `Median mean coreness` = "2.47",
+                   `Median % dangling nodes` = "24.99",
+                   `Median % well connected nodes` = "57.11",
+                   `Top 3 % counts` = "35.2",
+                   `Top 5 % counts` = "45.23",
+                   `Top 5 markers` = "CD44, B2M, CD59, CD45, CD43",
+                   `% Crossing edges (Initial)` = "5.06",
+                   `% Crossing edges (Refinement)` = "0.01"
+                 ),
+                 row.names = c(NA, -1L),
+                 class = c("tbl_df", "tbl", "data.frame")
+               ),
+             hashing =
+               structure(list(
+                 `Sample ID` = "S1", `Number of cells` = "30",
+                 `Number of cells >10k nodes` = "30", `Median isotype % counts` = "0.04",
+                 `Median intracellular % counts` = "0", `Median proteins per cell [k]` = "48.64",
+                 `Median reads per cell [k]` = "289.86", `% Denoised UMIs` = "4.78",
+                 `Total denoised UMIs [M]` = "0", `Median mean coreness` = "2.47",
+                 `Median % dangling nodes` = "24.99", `Median % well connected nodes` = "57.11",
+                 `Top 3 % counts` = "35.2", `Top 5 % counts` = "45.23", `Top 5 markers` = "CD44, B2M, CD59, CD45, CD43"
+               ), row.names = c(
+                 NA,
+                 -1L
+               ), class = c("tbl_df", "tbl", "data.frame"))
       )
     )
   })
