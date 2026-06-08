4e365b81ef44e2c3b86a155a6bc15e61564ab63e
maxkarlsson <max.karlsson@pixelgen.com>
2026-06-08 14:28:03 +0200

    fix: global binding

--- R/aaa.R
+++ R/aaa.R
@@ -50,7 +50,8 @@ globalVariables(
     "saturation_mean", "saturation_se",
     "pre_denoise_isotype_fraction", "umis_denoised", "isotype_reduction",
     "fraction_denoised", "median_isotype_reduction",
-    "median_pre_denoise_isotype", "median_post_denoise_isotype"
+    "median_pre_denoise_isotype", "median_post_denoise_isotype",
+    "x_pos", "y_pos"
   ),
   package = "pixelatorES",
   add = TRUE
